c23b2e241422f494ce631c118f1dc71bdaff7c89
jleous <jose-leonardo49@hotmail.com>
2023-06-27 17:06:49 -0400

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>Ts Amazona</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  )
+}
+
+export default App
